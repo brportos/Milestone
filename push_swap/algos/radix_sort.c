306e6decd4e46352f66ebc7f3f7f457ea4075373
brnportos <brnoportos@gmail.com>
2026-06-08 18:32:52 +0300

    feat(push_swap): add radix sort implementation

--- push_swap/algos/radix_sort.c
+++ push_swap/algos/radix_sort.c
@@ -21,3 +21,33 @@ int	get_max_bits(t_stack *a)
 		bits++;
 	return (bits);
 }
+
+void	radix_sort(t_stack **a, t_stack **b, t_stats *ops)
+{
+	int	size;
+	int	max_bits;
+	int	i;
+	int	j;
+
+	if (is_sorted(*a) == 1)
+		return ;
+	max_bits = get_max_bits(*a);
+	i = 0;
+	while (i < max_bits)
+	{
+		size = stack_size(*a);
+		j = 0;
+		while (j < size)
+		{
+			if ((((*a)->index >> i) & 1) == 0)
+				pb(a, b, ops);
+			else
+				ra(a, ops);
+			j++;
+		}
+		while (*b)
+			pa(a, b, ops);
+		i++;
+	}
+}
+
