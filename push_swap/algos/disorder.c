ac0867d52061cf678fd13251041f80fc6e2164d0
brportos <brnportos@gmail.com>
2026-06-09 19:02:19 +0300

    fix

--- push_swap/algos/disorder.c
+++ push_swap/algos/disorder.c
@@ -1,29 +1,3 @@
 #include "../push_swap.h"
 
-double	compute_disorder(t_stack *a, t_stats *ops)
-{
-	int		mistakes;
-	int		total_pairs;
-	t_stack	*i;
-	t_stack	*j;
 
-	mistakes = 0;
-	total_pairs = 0;
-	i = a;
-	if (!a)
-		return (0);
-	while (i && i->next)
-	{
-		j = i->next;
-		while (j)
-		{
-			if (i->content > j->content)
-				mistakes++;
-			total_pairs++;
-			j = j->next;
-		}
-		i = i->next;
-	}
-	ops->disorder = (double)mistakes / total_pairs;
-	return ((double)mistakes / total_pairs);
-}
